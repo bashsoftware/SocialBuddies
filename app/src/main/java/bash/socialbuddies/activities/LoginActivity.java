b5055b1c386bb9f53542c1fbfb2f62d12e5cbc35
bashsoftware <software.bash@gmail.com>
2018-09-29 16:34:52 -0500

    :v

--- app/src/main/java/bash/socialbuddies/activities/LoginActivity.java
+++ app/src/main/java/bash/socialbuddies/activities/LoginActivity.java
@@ -1,4 +1,4 @@
-package bash.socialbuddies;
+package bash.socialbuddies.activities;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -14,11 +15,15 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import bash.socialbuddies.R;
+import bash.socialbuddies.activities.MainActivity;
+
 public class LoginActivity extends AppCompatActivity {
 
     private EditText edtEmail;
     private EditText edtPass;
     private Button btnLogin;
+    private TextView txvRegistro;
 
     private FirebaseAuth auth;
 
@@ -35,6 +40,7 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin = findViewById(R.id.btnLogin);
         edtPass = findViewById(R.id.edtPass);
         edtEmail = findViewById(R.id.edtEmail);
+        txvRegistro = findViewById(R.id.txvRegistro);
     }
 
     private void initEvents(){
@@ -53,6 +59,13 @@ public class LoginActivity extends AppCompatActivity {
                             }
                         }
                 });
+            }
+        });
+
+        txvRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
             }
         });
     }
